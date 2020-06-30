a997ff02e6a1ebd8abce3d0bd39f2f696fa56bcf
Jean-Claude Jung <jjung@adobe.com>
2020-06-30 12:43:26 +0200

    Improvements

--- src/airp.py
+++ src/airp.py
@@ -14,18 +14,19 @@ AIRPD_PRODUCT_INDX = {
 
 }
 
+# automatic blueutil installer
 os.environ['PATH'] = os.popen('./_sharedresources "blueutil"').readline()
 
 
-def get_airpods(device_id: str) -> tuple:
+def airpod_info(device_id: str) -> tuple:
     """
-    Get device status with a given address (MAC)
+    Get airpod info with a given address (MAC)
 
     Args:
-        device_id (str): MAC address of the Device to search
+        device_id (str): MAC address of the AirPod to search
 
     Returns:
-        tuble: Left/Right/Case battery status
+        tuble: productlabel, productid, vendorid
     """
     with open("/Library/Preferences/com.apple.Bluetooth.plist", "rb") as f:
         pl = plistlib.load(f)
@@ -33,21 +34,26 @@ def get_airpods(device_id: str) -> tuple:
     ret: tuple = tuple()
     for d, v in devices.items():
         if device_id in d:
-            product_id: str = v.get("ProductID") if v.get("ProductID") else None
+            product_id: str = v.get("ProductID")
             product_label: str = AIRPD_PRODUCT_INDX.get(product_id)
-            vendor_id: str = v.get("VendorID") if v.get("VendorID") else None
+            vendor_id: str = v.get("VendorID")
             ret: tuple = (product_label, product_id, vendor_id)
             break
     return ret
 
 
-def get_avail_airpods() -> dict:
-    #jsn = json.loads(os.popen('/usr/local/bin/blueutil --paired --format json').read())
+def paired_airpods() -> dict:
+    """
+    Get paired AirPods including info
+
+    Returns:
+        dict: dict with paired AirPod name including dict with info
+    """
     jsn = json.loads(os.popen('blueutil --paired --format json').read())
     out_dict = {}
     for i in jsn:
         address = i.get('address')
-        prod_label, prod_id, vendor_id = get_airpods(address)
+        prod_label, prod_id, vendor_id = airpod_info(address)
         if vendor_id == 76 and prod_id in AIRPD_PRODUCT_INDX.keys():
             out_dict.update(
                 {i.get('name'):
@@ -61,6 +67,12 @@ def get_avail_airpods() -> dict:
 
 
 def is_blueutil() -> bool:
+    """
+    Checks if Blueutil was properly installed
+
+    Returns:
+        bool: True=available, False=not found
+    """
     blueutil = os.popen("blueutil -v").readline()
     if blueutil == "":
         return False
@@ -70,8 +82,7 @@ def is_blueutil() -> bool:
 
 wf = Items()
 if is_blueutil():
-    avail_airpods = get_avail_airpods()
-    for ap_name, status in avail_airpods.items():
+    for ap_name, status in paired_airpods().items():
         adr = status.get('address')
         ap_type = status.get('prod_label')
         is_connected = status.get('connected')
