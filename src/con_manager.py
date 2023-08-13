e263fd15c9bea689902ef399276315e54098313c
paraversal <104731964+paraversal@users.noreply.github.com>
2023-08-13 13:49:35 +0200

    add move audio functionality
    also removed connect/disconnect func, fixed airpods pro 2 images, updated ui

--- src/con_manager.py
+++ src/con_manager.py
@@ -2,16 +2,29 @@
 
 import os
 import shutil
+import sys
+import re
 
 from Alfred3 import Tools
 
 query = Tools.getArgv(1)
-adr, switch = tuple(query.split(";"))
-blueutil = shutil.which('blueutil')
+name, switch, adr = tuple(query.split(";"))
+adr_escaped = re.sub("\s", "\ ", adr)
+name_escaped = re.sub("\s", "\ ", name)
 
-if blueutil:
-    if switch == "disconnected":
-        Tools.log(adr)
-        os.popen(f'{blueutil} --connect {adr}')
-    else:
-        os.popen(f'{blueutil} --disconnect {adr} --wait-disconnect {adr}')
+blueutil = shutil.which('blueutil')
+switch_audio = os.environ["SwitchAudioSource_path"]
+
+# if blueutil doesn't exist, we can just quit right away
+if not blueutil:
+    sys.exit()
+
+# current behaviour: when the APs are not connected, we connect them with blueutil and move audio to them. If they are connected, we just move audio to them
+if switch == "disconnected":
+    Tools.log(name_escaped)
+    # this connects to the airpods
+    os.popen(f'{blueutil} --connect {adr_escaped}')
+    # this moves audio to the airpods
+    os.popen(f'{switch_audio} -s {name_escaped}')
+else:
+    os.popen(f'{switch_audio} -s {name_escaped}')
